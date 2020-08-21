df56fe2405095c2d36ee48c483282a399b5054e7
Eduardo Cabral <eduardosbcabral@gmail.com>
2020-08-20 23:38:57 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,18 +2,18 @@ from setuptools import setup
 
 
 setup(
-      name='my_custom_sklearn_transforms',
+      name='my_custom_sklearn_transforms_eduardosbcabral',
       version='1.0',
       description='''
             This is a sample python package for encapsulating custom
             tranforms from scikit-learn into Watson Machine Learning
       ''',
-      url='https://github.com/vnderlev/sklearn_transforms/',
-      author='Vanderlei Munhoz',
-      author_email='vnderlev@protonmail.ch',
+      url='https://github.com/eduardosbcabral/sklearn_transforms/',
+      author='Eduardo Cabral',
+      author_email='eduardosbcabral@gmail.com',
       license='BSD',
       packages=[
-            'my_custom_sklearn_transforms'
+            'my_custom_sklearn_transforms_eduardosbcabral'
       ],
       zip_safe=False
 )
