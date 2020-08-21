861948669439ae767c711c4e7b22a0056058e26b
Eduardo Cabral <eduardosbcabral@gmail.com>
2020-08-20 23:26:40 -0300

    add imports

--- my_custom_sklearn_transforms/sklearn_transformers.py
+++ my_custom_sklearn_transforms/sklearn_transformers.py
@@ -1,5 +1,7 @@
 from sklearn.base import BaseEstimator, TransformerMixin
-
+import imblearn
+from imblearn.over_sampling import SMOTE
+import pandas as pd
 
 # All sklearn Transforms must have the `transform` and `fit` methods
 class DropColumns(BaseEstimator, TransformerMixin):
